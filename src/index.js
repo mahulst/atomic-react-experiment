0fe97ca67f9a5e41a9e81395ec2cb2b80f4764f2
Michel van der Hulst <michel@voorkanter.com>
2018-01-14 15:28:32 +0100

    Add code highlighting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import '../node_modules/highlight.js/styles/ocean.css';
 import Docs from './docs/Docs';
-
 ReactDOM.render(<Docs />, document.getElementById('root'));
