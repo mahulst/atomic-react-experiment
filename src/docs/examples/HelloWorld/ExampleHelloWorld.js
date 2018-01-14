056338e2df6e1408cef96e1f32584cc215e4df43
Michel van der Hulst <michel@voorkanter.com>
2018-01-14 15:26:31 +0100

    Initial setup

---
+++ src/docs/examples/HelloWorld/ExampleHelloWorld.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import HelloWorld from 'ps-react/HelloWorld';
+
+
+export default function ExampleHelloWorld() {
+  return <HelloWorld message='Hello!'/>
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Docs from './docs/Docs';
+
+ReactDOM.render(<Docs />, document.getElementById('root'));
